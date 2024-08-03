3263eaed350c456419a4526379c2a19bea8bc7e4
The Altro <albe020531@outlook.com>
2024-08-03 11:03:31 -0400

    fix: agredado cors

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { MiembrosModule } from './miembros/miembros.module';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get('DB_HOST'),
-        port: +configService.get('DB_PORT'),
+        port: configService.get('DB_PORT'),
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
