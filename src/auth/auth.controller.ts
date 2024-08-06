19961c9eb466061b7978da726e42a7c207bf3c26
The Altro <albe020531@outlook.com>
2024-08-06 17:21:51 -0400

    fix: algunos cambios
    - Arregladas importaciones en el archivo auth.controller.ts
    - Agregado endpoint para obtener el Perfil

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Post, Req, HttpStatus, HttpCode } from '@nestjs/common';
+import { Controller, Post, Req, HttpStatus, HttpCode, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express';
+import { AuthGuard } from './guards/auth.guard';
+import { User } from 'src/user/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @HttpCode(HttpStatus.ACCEPTED)
   @Post('authentication')
   async authentication(@Req() req: Request) {
     const { name, password } = req.body;
     return this.authService.authentication(name, password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Req() req: Request & { user: User }) {
+    return req.user;
   }
 }
