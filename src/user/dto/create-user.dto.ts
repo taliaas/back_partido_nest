50cbf8707a061e8e745c95041dea4270cda3b164
taliaas <taliacarruesalas@gmail.com>
2024-08-07 18:22:31 -0400

    create

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,5 @@
+import { Role } from 'src/role/entities/role.entity';
+
 export class CreateUserDto {
   name: string;
 
@@ -5,5 +7,5 @@ export class CreateUserDto {
 
   password: string;
 
-  role: number;
+  role: Role[];
 }
