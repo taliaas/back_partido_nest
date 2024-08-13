f2f5491d9b11e369f1055527731aaabc4e91407a
@n0n5399 <andyvergaracardoza@gmail.com>
2024-08-13 15:23:08 -0400

    Funcion para eliminar el blance. Funcion para actualizar el balance

--- src/balance/balance.service.ts
+++ src/balance/balance.service.ts
@@ -64,6 +64,12 @@ export class BalanceService {
     return await this.balanceRepository.remove(bal);
   }
 
+  //funcion para eliminar el balance
+  async deleteBalanceByActaROId(actaRoId: number): Promise<void> {
+    await this.balanceRepository.delete({ minutes: actaRoId });
+  }
+
+  //funcion para crear el balance 
   async createBalance(actaRoId: number) {
   
     const actaRo = await this.actaRORepository.findOne({ where: { id: actaRoId } });
@@ -88,6 +94,35 @@ export class BalanceService {
       crecim: crecimValue,
     });
   
+    
+    await this.balanceRepository.save(balance);
+  }
+
+  //Funcion para actualizar el balance 
+  async updateBalanceByActaROId(actaRoId: number): Promise<void> {
+    const actaRo = await this.actaRORepository.findOne({ where: { id: actaRoId } });
+    if (!actaRo) {
+      throw new Error('Acta RO not found');
+    }
+
+    const balance = await this.balanceRepository.findOne({ where: { minutes: actaRoId } });
+    if (!balance) {
+      throw new Error('Balance not found');
+    }
+
+    // Actualiza los valores del balance basados en el acta actualizada
+    balance.core = actaRo.nucleo;
+    balance.order = (actaRo.order.match(/desc/g) || []).length;
+    balance.participants = actaRo.present;
+    balance.agreements = (actaRo.agreements.match(/desc/g) || []).length;
+    
+    const crecimValue = await this.findCreciment(actaRo.development);
+    balance.crecim = crecimValue;
+    
+    const actaCp = await this.actaCpRepository.findOne({ where: { idactaro: actaRoId } });
+    balance.cp = actaCp ? 1 : 0;
+    balance.agreements_cp = actaCp ? (actaCp.agreements.match(/desc/g) || []).length : 0;
+
     
     await this.balanceRepository.save(balance);
   }
