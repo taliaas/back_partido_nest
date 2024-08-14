bc015dd01c1fd77a1f5f5f5186b32d1bb35665de
taliaas <taliacarruesalas@gmail.com>
2024-08-14 10:38:52 -0400

    update balance

--- src/balance/balance.service.ts
+++ src/balance/balance.service.ts
@@ -65,8 +65,41 @@ export class BalanceService {
       month: month,
       crecim: crecimValue,
     });
-  
-    
+    await this.balanceRepository.save(balance);
+  }
+
+  async updateBalanceByActaROId(actaRoId: number): Promise<void> {
+    const actaRo = await this.actaRORepository.findOne({
+      where: { id: actaRoId },
+    });
+    if (!actaRo) {
+      throw new Error('Acta RO not found');
+    }
+
+    const balance = await this.balanceRepository.findOne({
+      where: { minutes: actaRoId },
+    });
+    if (!balance) {
+      throw new Error('Balance not found');
+    }
+
+    // Actualiza los valores del balance basados en el acta actualizada
+    balance.core = actaRo.nucleo;
+    balance.order = (actaRo.order.match(/desc/g) || []).length;
+    balance.participants = actaRo.present;
+    balance.agreements = (actaRo.agreements.match(/desc/g) || []).length;
+
+    const crecimValue = await this.findCreciment(actaRo.development);
+    balance.crecim = crecimValue;
+
+    const actaCp = await this.actaCpRepository.findOne({
+      where: { idRO: actaRoId },
+    });
+    balance.cp = actaCp ? 1 : 0;
+    balance.agreements_cp = actaCp
+      ? (actaCp.agreements.match(/desc/g) || []).length
+      : 0;
+
     await this.balanceRepository.save(balance);
   }
 }
